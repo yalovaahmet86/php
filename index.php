7748a41ca5208cd1056e19a093c03b06f76f7c74
yalovaahmet86 <yalovaahmet86@gmail.com>
2025-12-08 18:27:18 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,27 @@
 <?php echo "merhaba" ?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Title of the document</title>
+  <script>
+  // Initialize the agent on page load.
+  const fpPromise = import('https://fpjscdn.net/v3/0tZ76oEWXRmzVUIRvxfU')
+    .then(FingerprintJS => FingerprintJS.load({
+      region: "ap"
+    }))
+
+  // Get the visitorId when you need it.
+  fpPromise
+    .then(fp => fp.get())
+    .then(result => {
+      const visitorId = result.visitorId
+      console.log(visitorId)
+    })
+</script>
+</head>
+
+<body>
+The content of the document......
+</body>
+
+</html>
